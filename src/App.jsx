29de1b08aa83ed138971b127df8bc569a64ed7f3
memmer57 <matej.memmer@gmail.com>
2022-11-13 17:53:49 +0100

    Add items to TODO list

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,48 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import TodoItem from './TODO item/TodoItem'
 import CreateItem from './TODO_create_item/CreateItem'
+import { useState } from 'react'
 
 function App() {
+  const [popupVisibility, setPopupVisibility] = useState(false)
+  const editPopupVisibility = (e) => {
+    setPopupVisibility(e)
+  }
+
+  let itemCount = parseInt(localStorage.getItem('0'))
+  let itemName = []
+  let itemDescription = []
+
+  for (let i = 1; i <= itemCount; i++) {
+    let nameKey = i + 'name'
+    let descriptionKey = i + 'description'
+
+    itemName.push(localStorage.getItem(nameKey))
+    itemDescription.push(localStorage.getItem(descriptionKey))
+  }
+
+  console.log(itemName)
+  console.log(itemDescription)
+
+
+  const itemList = itemName.map((name, index) => {
+    return <TodoItem name={name} description={itemDescription[index]} key={index} />
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={reactLogo} className="App-logo" alt="logo" />
         <h2>React TODO list</h2>
-        <button>Create new item</button>
+
+        <button onClick={() => setPopupVisibility(!popupVisibility)}>Create new item</button>
+        
       </header>
 
-      <TodoItem name="Vyklidit mycku" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque pretium lectus id turpis. Quisque porta. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus." />
-      <TodoItem name="Vynest kos" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque pretium lectus id turpis. Quisque porta. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus." />
-      <TodoItem name="Udelat ukol na matiku" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque pretium lectus id turpis. Quisque porta. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus." />
+      {popupVisibility && <CreateItem editPopup={editPopupVisibility}/>}
+
+      {itemList}
+
     </div>
   )
 }
--- src/TODO_create_item/CreateItem.jsx
+++ src/TODO_create_item/CreateItem.jsx
@@ -1,15 +1,45 @@
 import './CreateItem.css'
+import App from '../App'
+import { useState } from 'react'
 
-function CreateItem() {
+function CreateItem(props) {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // increment item count
+    if (localStorage.getItem('0') === null) {
+      localStorage.setItem('0', 1)
+    } else {
+      let newKey = parseInt(localStorage.getItem('0')) + 1
+      localStorage.setItem('0', newKey)
+    }
+
+    let itemCount = localStorage.getItem('0')
+    let nameKey = itemCount + 'name'
+    let descriptionKey = itemCount + 'description'
+
+    localStorage.setItem(nameKey, name)
+    localStorage.setItem(descriptionKey, description)
+
+    props.editPopup(false);
+    console.log('submit')
+  }
+
+  
   return (
     <div className="create-item">
-        <h2>Create new TODO item</h2>
-        <form className='create-item-form'>
+        <form onSubmit={handleSubmit} className='create-item-form'>
+            <button class="close-button" onClick={() => props.editPopup(false)}>X</button>
+
+            <h2>Create new TODO item</h2>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Name" />
+            <input type="text" value={name} onChange={e => setName(e.target.value)} id="name" name="name" placeholder="Name" />
 
             <label htmlFor="description">Description</label>
-            <textarea id="description" name="description" placeholder="Description" />
+            <textarea value={description} onChange={e => setDescription(e.target.value)} id="description" name="description" placeholder="Description" />
 
             <button type="submit">Create</button>
         </form>
